26548135503ca7049af12ae7843fa516543af82e
Daniel Bonhaure <danielbonhaure@gmail.com>
2018-10-08 23:53:43 -0300

    Update Install.R

--- core/modules/data_updater/impute_script/Install.R
+++ core/modules/data_updater/impute_script/Install.R
@@ -1,4 +1,4 @@
-options(repos=c(CRAN="http://lib.stat.cmu.edu/R/CRAN/"))
+options(repos=c(CRAN="http://cran.stat.ucla.edu/"))
 
 suppressPackageStartupMessages(if(!require('lazyeval')) { install.packages("lazyeval", dependencies=TRUE, quiet=TRUE); require('lazyeval') })  # sudo apt install libssl-dev
 suppressPackageStartupMessages(if(!require('sirad')) { install.packages("sirad", dependencies=TRUE, quiet=TRUE); require('sirad') })
