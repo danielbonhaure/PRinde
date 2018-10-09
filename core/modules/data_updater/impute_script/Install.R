78f073dca74e11a1c889ba311155fc4fad496271
Daniel Bonhaure <danielbonhaure@gmail.com>
2018-10-08 23:39:38 -0300

    Update Install.R

--- core/modules/data_updater/impute_script/Install.R
+++ core/modules/data_updater/impute_script/Install.R
@@ -1,12 +1,12 @@
 options(repos=c(CRAN="http://lib.stat.cmu.edu/R/CRAN/"))
 
-suppressPackageStartupMessages(if(!require('lazyeval')) { install.packages("lazyeval", dependencies=TRUE); require('lazyeval') }) # sudo apt install libcurl4-openssl-dev
-suppressPackageStartupMessages(if(!require('sirad')) { install.packages("sirad", dependencies=TRUE); require('sirad') }) # sudo apt install udunits-bin libudunits2-0 libudunits2-dev
-suppressPackageStartupMessages(if(!require('missForest')) { install.packages("missForest", dependencies=TRUE); require('missForest') })
-suppressPackageStartupMessages(if(!require('doMC')) { install.packages("doMC", dependencies=TRUE); require('doMC') })
-suppressPackageStartupMessages(if(!require('gstat')) { install.packages("gstat"); require('gstat') }) # sudo apt install libgeos-dev
-suppressPackageStartupMessages(if(!require('geosphere')) { install.packages("geosphere"); require('geosphere') })
-suppressPackageStartupMessages(if(!require('rgdal')) { install.packages("rgdal"); require('rgdal') });  # sudo apt install libproj-dev libgdal-dev
-suppressPackageStartupMessages(if(!require('optparse')) { install.packages("optparse", dependencies=TRUE); require('optparse') });
-suppressPackageStartupMessages(if(!require('xts')) { install.packages("xts"); require('xts'); })
-suppressPackageStartupMessages(if(!require('dplyr')) { install.packages("dplyr", dependencies=TRUE); require('dplyr') });
+suppressPackageStartupMessages(if(!require('lazyeval')) { install.packages("lazyeval", dependencies=TRUE, quiet=TRUE); require('lazyeval') })  # sudo apt install libssl-dev
+suppressPackageStartupMessages(if(!require('sirad')) { install.packages("sirad", dependencies=TRUE, quiet=TRUE); require('sirad') })
+suppressPackageStartupMessages(if(!require('missForest')) { install.packages("missForest", dependencies=TRUE, quiet=TRUE); require('missForest') })
+suppressPackageStartupMessages(if(!require('doMC')) { install.packages("doMC", dependencies=TRUE, quiet=TRUE); require('doMC') })
+suppressPackageStartupMessages(if(!require('gstat')) { install.packages("gstat", dependencies=TRUE, quiet=TRUE); require('gstat') })
+suppressPackageStartupMessages(if(!require('geosphere')) { install.packages("geosphere", dependencies=TRUE, quiet=TRUE); require('geosphere') })
+suppressPackageStartupMessages(if(!require('rgdal')) { install.packages("rgdal", dependencies=TRUE, quiet=TRUE); require('rgdal') });  # sudo apt install libgdal-dev
+suppressPackageStartupMessages(if(!require('optparse')) { install.packages("optparse", dependencies=TRUE, quiet=TRUE); require('optparse') });
+suppressPackageStartupMessages(if(!require('xts')) { install.packages("xts", dependencies=TRUE, quiet=TRUE); require('xts'); })
+suppressPackageStartupMessages(if(!require('dplyr')) { install.packages("dplyr", dependencies=TRUE, quiet=TRUE); require('dplyr') });
