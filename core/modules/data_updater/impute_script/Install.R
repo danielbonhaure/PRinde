5883afe9ce3fc8e373763af98309ebac91f9f666
Daniel Bonhaure <danielbonhaure@gmail.com>
2018-10-26 18:19:33 -0300

    Update Install.R
    The dependencies installation (dependencies=TRUE) is removed, except when Fede Shmidt was used this option.

--- core/modules/data_updater/impute_script/Install.R
+++ core/modules/data_updater/impute_script/Install.R
@@ -4,9 +4,10 @@ suppressPackageStartupMessages(if(!require('lazyeval')) { install.packages("lazy
 suppressPackageStartupMessages(if(!require('sirad')) { install.packages("sirad", dependencies=TRUE, quiet=TRUE); require('sirad') })
 suppressPackageStartupMessages(if(!require('missForest')) { install.packages("missForest", dependencies=TRUE, quiet=TRUE); require('missForest') })
 suppressPackageStartupMessages(if(!require('doMC')) { install.packages("doMC", dependencies=TRUE, quiet=TRUE); require('doMC') })
-suppressPackageStartupMessages(if(!require('gstat')) { install.packages("gstat", dependencies=TRUE, quiet=TRUE); require('gstat') })  # dep rgeos installation fails, that's don't matter, see the gstat doc
-suppressPackageStartupMessages(if(!require('geosphere')) { install.packages("geosphere", dependencies=TRUE, quiet=TRUE); require('geosphere') })
-suppressPackageStartupMessages(if(!require('rgdal')) { install.packages("rgdal", dependencies=TRUE, quiet=TRUE); require('rgdal') });  # sudo apt install libgdal-dev
+suppressPackageStartupMessages(if(!require('gstat')) { install.packages("rgeos", quiet=TRUE); require('rgeos') })  # sudo apt install libgeos++-dev
+suppressPackageStartupMessages(if(!require('gstat')) { install.packages("gstat", quiet=TRUE); require('gstat') })
+suppressPackageStartupMessages(if(!require('geosphere')) { install.packages("geosphere", quiet=TRUE); require('geosphere') })
+suppressPackageStartupMessages(if(!require('rgdal')) { install.packages("rgdal", quiet=TRUE); require('rgdal') });  # sudo apt install libgdal-dev
 suppressPackageStartupMessages(if(!require('optparse')) { install.packages("optparse", dependencies=TRUE, quiet=TRUE); require('optparse') });
-suppressPackageStartupMessages(if(!require('xts')) { install.packages("xts", dependencies=TRUE, quiet=TRUE); require('xts'); })
+suppressPackageStartupMessages(if(!require('xts')) { install.packages("xts", quiet=TRUE); require('xts'); })
 suppressPackageStartupMessages(if(!require('dplyr')) { install.packages("dplyr", dependencies=TRUE, quiet=TRUE); require('dplyr') });
