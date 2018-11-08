7bd601e747599e5b5e7e710f80e016812bcf5ce3
Daniel Bonhaure <danielbonhaure@gmail.com>
2018-11-08 20:11:17 -0300

    Changes for install R packages without dependencies.

--- core/modules/data_updater/impute_script/Install.R
+++ core/modules/data_updater/impute_script/Install.R
@@ -1,13 +1,13 @@
 options(repos=c(CRAN="https://cran.r-project.org/"))
 
-suppressPackageStartupMessages(if(!require('lazyeval')) { install.packages("lazyeval", dependencies=TRUE, quiet=TRUE); require('lazyeval') })  # sudo apt install libssl-dev
-suppressPackageStartupMessages(if(!require('sirad')) { install.packages("sirad", dependencies=TRUE, quiet=TRUE); require('sirad') })
-suppressPackageStartupMessages(if(!require('missForest')) { install.packages("missForest", dependencies=TRUE, quiet=TRUE); require('missForest') })
-suppressPackageStartupMessages(if(!require('doMC')) { install.packages("doMC", dependencies=TRUE, quiet=TRUE); require('doMC') })
+suppressPackageStartupMessages(if(!require('lazyeval')) { install.packages("lazyeval", quiet=TRUE); require('lazyeval') })  # sudo apt install libssl-dev
+suppressPackageStartupMessages(if(!require('sirad')) { install.packages("sirad", quiet=TRUE); require('sirad') })
+suppressPackageStartupMessages(if(!require('missForest')) { install.packages("missForest", quiet=TRUE); require('missForest') })
+suppressPackageStartupMessages(if(!require('doMC')) { install.packages("doMC", quiet=TRUE); require('doMC') })
 suppressPackageStartupMessages(if(!require('rgeos')) { install.packages("rgeos", quiet=TRUE); require('rgeos') })  # sudo apt install libgeos++-dev
 suppressPackageStartupMessages(if(!require('gstat')) { install.packages("gstat", quiet=TRUE); require('gstat') })
 suppressPackageStartupMessages(if(!require('geosphere')) { install.packages("geosphere", quiet=TRUE); require('geosphere') })
 suppressPackageStartupMessages(if(!require('rgdal')) { install.packages("rgdal", quiet=TRUE); require('rgdal') });  # sudo apt install libgdal-dev
-suppressPackageStartupMessages(if(!require('optparse')) { install.packages("optparse", dependencies=TRUE, quiet=TRUE); require('optparse') });
+suppressPackageStartupMessages(if(!require('optparse')) { install.packages("optparse", quiet=TRUE); require('optparse') });
 suppressPackageStartupMessages(if(!require('xts')) { install.packages("xts", quiet=TRUE); require('xts'); })
-suppressPackageStartupMessages(if(!require('dplyr')) { install.packages("dplyr", dependencies=TRUE, quiet=TRUE); require('dplyr') });
+suppressPackageStartupMessages(if(!require('dplyr')) { install.packages("dplyr", quiet=TRUE); require('dplyr') });
