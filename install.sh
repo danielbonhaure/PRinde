4a92d16526c831f5c123ef8595bde6aa9f1d6d1c
danielbonhaure <danielbonhaure@gmail.com>
2019-01-23 19:09:05 -0300

    Improved installation and setup scripts

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@
 # wget https://raw.githubusercontent.com/danielbonhaure/PRinde/master/install.sh --output-document=install-prinde.sh
 #
 
+new_script "Install ProRindeS"
+
+
+new_section "1- Check if gutils.sh exists. If not, it will be downloaded!"
 
 if [[ ! -f gutils.sh ]]; then
     wget https://raw.githubusercontent.com/danielbonhaure/PRinde/master/gutils.sh
@@ -11,13 +15,8 @@ fi
 
 source gutils.sh; if [[ $? -ne 0 ]] ; then exit 1; fi
 
-#
-#
-#
 
-new_script "Install ProRindeS"
-
-new_section "1- Check if psims is already instaled!"
+new_section "2- Check if psims is already instaled!"
 
 if [[ ! -d /opt/psims ]]; then
     report_error "ERROR: psims has not yet been installed!! See: https://github.com/danielbonhaure/psims"
@@ -25,7 +24,7 @@ if [[ ! -d /opt/psims ]]; then
 fi
 
 
-new_section "2- Remove old instalation (if exist)"
+new_section "3- Remove old instalation (if exist)"
 
 if [[ -d /opt/prorindes ]]; then
     sudo rm -rf /opt/prorindes
@@ -36,7 +35,8 @@ sudo mkdir /opt/prorindes
 sudo chown "${USER}":"${USER}" /opt/prorindes
 sudo chmod g+w /opt/prorindes
 
-new_section "2- Clone repository"
+
+new_section "4- Clone ProRindeS github repository"
 git clone https://github.com/danielbonhaure/PRinde.git /opt/prorindes
 if [[ $? -ne 0 ]] ; then exit 1; fi
 
